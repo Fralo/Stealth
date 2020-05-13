9fea8a01a26f71b916ddd80bd6a82f9a4d388a9c
Fralo <leonardo.fralini@gmail.com>
2020-05-14 01:04:30 +0200

    Real fix of AStar pt 3

--- src/AStar.cpp
+++ src/AStar.cpp
@@ -14,11 +14,11 @@ std::vector <Node> AStar::getPath(Node hunter, Node dest) {
         //You are the destination
         return empty;
     }
-    bool closedList[mapSize.x-1][mapSize.y-1];
+    bool closedList[mapSize.x - 1][mapSize.y - 1];
     //Get the overview of the map
-    Node allMap[mapSize.x-1][mapSize.y-1];
-    for (int x = 1; x < (mapSize.x-1); x++) {
-        for (int y = 1; y < (mapSize.y-1); y++) {
+    Node allMap[mapSize.x - 1][mapSize.y - 1];
+    for (int x = 1; x < (mapSize.x - 1); x++) {
+        for (int y = 1; y < (mapSize.y - 1); y++) {
             allMap[x][y].fCost = FLT_MAX;
             allMap[x][y].gCost = FLT_MAX;
             allMap[x][y].hCost = FLT_MAX;
@@ -32,8 +32,8 @@ std::vector <Node> AStar::getPath(Node hunter, Node dest) {
     }
 
     //Initialize our starting list
-    int x = hunter.x/mapSize.x;
-    int y = hunter.y/mapSize.y;
+    int x = hunter.x / mapSize.x;
+    int y = hunter.y / mapSize.y;
     allMap[x][y].fCost = 0.0;
     allMap[x][y].gCost = 0.0;
     allMap[x][y].hCost = 0.0;
@@ -44,7 +44,7 @@ std::vector <Node> AStar::getPath(Node hunter, Node dest) {
     openList.emplace_back(allMap[x][y]);
     bool destinationFound = false;
 
-    while (!openList.empty() && openList.size() < (mapSize.x-1) * (mapSize.y-1)) {
+    while (!openList.empty() && openList.size() < (mapSize.x - 1) * (mapSize.y - 1)) {
         Node node;
         do {
             float temp = FLT_MAX;
@@ -64,8 +64,8 @@ std::vector <Node> AStar::getPath(Node hunter, Node dest) {
             openList.erase(itNode);
         } while (!isValid(node.x, node.y));
 
-        x = node.x/mapSize.x;
-        y = node.y/mapSize.y;
+        x = node.x / mapSize.x;
+        y = node.y / mapSize.y;
         closedList[x][y] = true;
 
         //For each neighbour starting from North-West to South-East
@@ -73,29 +73,29 @@ std::vector <Node> AStar::getPath(Node hunter, Node dest) {
             for (int newY = -1; newY <= 1; newY++) {
                 double gNew, hNew, fNew;
                 //trying to deny diagonals
-                if ((abs(newX) != abs(newY)) && isValid(x + newX, y + newY)) {
-                    if (isDestination(x + newX, y + newY, dest)) {
-                        //Destination found - make path
+
+                if (isDestination(x + newX, y + newY, dest)) {
+                    //Destination found - make path
+                    allMap[x + newX][y + newY].parentX = x;
+                    allMap[x + newX][y + newY].parentY = y;
+                    destinationFound = true;
+                    return makePath(*allMap, dest);
+                } else if (!closedList[x + newX][y + newY]) {
+                    gNew = node.gCost + 1.0;
+                    hNew = calculateH(x + newX, y + newY, dest);
+                    fNew = gNew + hNew;
+                    // Check if this path is better than the one already present
+                    if (allMap[x + newX][y + newY].fCost == FLT_MAX ||
+                        allMap[x + newX][y + newY].fCost > fNew) {
+                        // Update the details of this neighbour node
+                        allMap[x + newX][y + newY].fCost = fNew;
+                        allMap[x + newX][y + newY].gCost = gNew;
+                        allMap[x + newX][y + newY].hCost = hNew;
                         allMap[x + newX][y + newY].parentX = x;
                         allMap[x + newX][y + newY].parentY = y;
-                        destinationFound = true;
-                        return makePath(*allMap, dest);
-                    } else if (!closedList[x + newX][y + newY]) {
-                        gNew = node.gCost + 1.0;
-                        hNew = calculateH(x + newX, y + newY, dest);
-                        fNew = gNew + hNew;
-                        // Check if this path is better than the one already present
-                        if (allMap[x + newX][y + newY].fCost == FLT_MAX ||
-                            allMap[x + newX][y + newY].fCost > fNew) {
-                            // Update the details of this neighbour node
-                            allMap[x + newX][y + newY].fCost = fNew;
-                            allMap[x + newX][y + newY].gCost = gNew;
-                            allMap[x + newX][y + newY].hCost = hNew;
-                            allMap[x + newX][y + newY].parentX = x;
-                            allMap[x + newX][y + newY].parentY = y;
-                            openList.emplace_back(allMap[x + newX][y + newY]);
-                        }
+                        openList.emplace_back(allMap[x + newX][y + newY]);
                     }
+
                 }
             }
         }
@@ -110,19 +110,20 @@ std::vector <Node> AStar::makePath(Node *map, Node dest) {
     try {
         int x = dest.x;
         int y = dest.y;
-        std::stack <Node> path;
-        std::vector <Node> usablePath;
+        std::stack<Node> path;
+        std::vector<Node> usablePath;
 
-        while (!(map[x * mapSize.y - 1 + y].parentX == x && map[x * mapSize.y - 1 + y].parentY == y)
-               && map[x * mapSize.y - 1 + y].x != -1 && map[x * mapSize.y - 1 + y].y != -1) {
-            path.push(map[x * mapSize.y - 1 + y]);
-            int tempX = map[x * mapSize.y - 1 + y].parentX;
-            int tempY = map[x * mapSize.y - 1 + y].parentY;
+        while (!(map[x * ((mapSize.y-1))+ y].parentX == x && map[x * ((mapSize.y-1))+ y].parentY == y)
+               && map[x * ((mapSize.y-1))+ y].x != -1 && map[x * ((mapSize.y-1))+ y].y != -1)
+        {
+            path.push(map[x * (mapSize.y-1)+ y]);
+            int tempX = map[x * (mapSize.y-1)+ y].parentX;
+            int tempY = map[x * (mapSize.y-1)+ y].parentY;
             x = tempX;
             y = tempY;
 
         }
-        path.push(map[x * mapSize.y - 1 + y]);
+        path.push(map[x * (mapSize.y-1)+ y]);
 
         while (!path.empty()) {
             Node top = path.top();
@@ -131,7 +132,7 @@ std::vector <Node> AStar::makePath(Node *map, Node dest) {
         }
         return usablePath;
     }
-    catch (const std::exception &e) {
+    catch(const std::exception& e){
         std::cout << e.what() << std::endl;
     }
 }
