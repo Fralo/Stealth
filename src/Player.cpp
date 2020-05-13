baf20ba26664e6bd9e6bda0cc04fd68eb2cf4ade
Fralo <leonardo.fralini@gmail.com>
2020-05-13 22:52:52 +0200

    Fixed plyer loading

--- src/Player.cpp
+++ src/Player.cpp
@@ -15,4 +15,11 @@ void Player::draw(sf::RenderTarget &target, sf::RenderStates states) const {
 Player::Player(sf::Vector2i position, Weapon weapon) : weapon(weapon) {
     this->position = position;
 
+    std::cout << "Created Player" << std::endl
+              << "  Position " << std::endl
+              << "     x: " << position.x << std::endl
+              << "     y: " << position.y << std::endl
+              << "  Weapon:" << std::endl
+              << "     Rate:   " << weapon.rate << std::endl
+              << "     Damage: " << weapon.damage << std::endl;
 }
