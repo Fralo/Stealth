8feb2c92d0e80da9bd2a625727b954e9bc28b35c
mattiamarilli <mattiamarilli@gmail.com>
2020-05-14 22:04:14 +0200

    working on seek

--- src/SeekStrategy.cpp
+++ src/SeekStrategy.cpp
@@ -13,10 +13,21 @@ sf::Vector2f SeekStrategy::getNextMove(GameObject &gameObject, Game &game) {
 sf::Vector2f nextMove;
 
 if(locations.size() != 0) {
+    if ((gameObject.position.x/ game.map->getTileSize().x)== (locations.at(currentTarget).x / game.map->getTileSize().x) &&
+        (gameObject.position.y/game.map->getTileSize().y) == (locations.at(currentTarget).y / game.map->getTileSize().y))
+    {
+
+        std::cout<<"Arrived To {x = "<<gameObject.position.x / game.map->getTileSize().x<< "} y = { "<<gameObject.position.y/ game.map->getTileSize().x<<std::endl;
+        currentTarget++;
+        currentTarget = currentTarget % locations.size();
+        std::cout<<"Go To {x = "<<locations.at(currentTarget).x << "} y = { "<<locations.at(currentTarget).y<<std::endl;
+    }
+
     aStar = new AStar(game.obstacles, game.map->getMapSize(), game.map->getTileSize());
 
     Node from = {gameObject.position.x, gameObject.position.y};
     Node to = {locations.at(currentTarget).x,locations.at(currentTarget).y};
+
 
     std::vector<Node> path = aStar->getPath(from, to);
     Node next = path.at(1);
@@ -24,22 +35,11 @@ if(locations.size() != 0) {
     if (!path.empty()) {
         nextMove.x = float(next.x * game.map->getTileSize().x) - from.x;
         nextMove.y = float(next.y * game.map->getTileSize().y) - from.y;
+        return nextMove;
     }
-
-    if (next.x == (to.x/game.map->getTileSize().x) && next.y == (to.y/game.map->getTileSize().y)) {
-
-        std::cout<<"Location n. "<<currentTarget<<std::endl;
-        std::cout<<"Arrived to : { x = " <<to.x<< " y = "<<to.y<<"}"<<std::endl;
-        currentTarget++;
-        currentTarget = currentTarget % locations.size();
 
 
     }
-    return nextMove;
-}
-    //std::cout << nextMove.x <<" " << nextMove.y <<std::endl;
-
-
 }
 
 
