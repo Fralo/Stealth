82443b0faaa70c0562dfdaa9f7ff1f1ae7c0cfdb
mattiamarilli <mattiamarilli@gmail.com>
2020-05-19 21:54:43 +0200

    started design player inventory

---
+++ src/Inventory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mattia on 19/05/20.
+//
+
+#include "Inventory.hpp"
---
+++ src/Inventory.hpp
@@ -0,0 +1,19 @@
+//
+// Created by mattia on 19/05/20.
+//
+
+#ifndef STEALTH_INVENTORY_HPP
+#define STEALTH_INVENTORY_HPP
+
+#include <iostream>
+#include <vector>
+
+class Inventory {
+public:
+    std::vector<int> explosives;
+
+
+};
+
+
+#endif //STEALTH_INVENTORY_HPP
