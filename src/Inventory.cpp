f0667d2bfdf1c52724ded1fd37c740cc7fe42ee4
Fralo <leonardo.fralini@gmail.com>
2020-08-11 19:07:24 +0200

    First test of item in inventroy

--- src/Inventory.cpp
+++ src/Inventory.cpp
@@ -4,11 +4,24 @@
 
 #include "Inventory.hpp"
 
+Inventory::Inventory() {
+    InventoryItem test;
+    test.id = 12;
+    this->inventory.push_front(test);
+}
 
 
 void Inventory::draw(sf::RenderTarget &target, sf::RenderStates states) const {
-    sf::RectangleShape liferect({static_cast<float>(40),200});
+    sf::RectangleShape liferect({static_cast<float>(50),150});
     liferect.setFillColor(sf::Color(200,200,0));
-    liferect.setPosition( target.getView().getCenter().x + 350,target.getView().getCenter().y - 250);
+    liferect.setPosition( target.getView().getCenter().x + 345,target.getView().getCenter().y - 275);
     target.draw(liferect);
+
+    for(InventoryItem i : inventory) {
+        sf::RectangleShape liferect({40,40});
+        liferect.setFillColor(sf::Color(100,200,0));
+        liferect.setPosition( target.getView().getCenter().x + 345,target.getView().getCenter().y - 275);
+        target.draw(liferect);
+        std::cout<<i.id<<std::endl;
+    }
 }
